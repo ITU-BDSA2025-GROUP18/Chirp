95f946b781e07c9f334b3c7da36076beb7345cc9
ITU-ahms <knahse4@knorddoc.dk>
2025-10-20 18:43:53 +0200

    test(factory): add WebAppFactory skeleton

--- tests/Chirp.Razor.Tests/WebAppFactory.cs
+++ tests/Chirp.Razor.Tests/WebAppFactory.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 public class WebAppFactory : WebApplicationFactory<Chirp.Razor.Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.UseEnvironment("Test");
-        // DB peges via CHIRPDBPATH i testene.
-    }
+    // udfyldes i de næste commits
 }
