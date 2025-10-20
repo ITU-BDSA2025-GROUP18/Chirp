835b4b8f1573dfb056fe2ef906a91c387535d864
ITU-ahms <knahse4@knorddoc.dk>
2025-10-20 18:45:51 +0200

    test(factory): hold in-memory DB alive via private field

--- tests/Chirp.Razor.Tests/WebAppFactory.cs
+++ tests/Chirp.Razor.Tests/WebAppFactory.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 public class WebAppFactory : WebApplicationFactory<Chirp.Razor.Program>
 {
+    private InMemorySqlite<ChirpDBContext>? _mem;
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test");
         // more config follows
     }
 }
+
 
 
