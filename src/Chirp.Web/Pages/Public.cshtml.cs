1dba4251f4502b8dd07c2c1063a3d535afc73bcd
erze <228920027+EranZejnelovski@users.noreply.github.com>
2025-10-23 23:16:39 +0200

    Importing EFCore & Adding ref from Chirp.Database to Chirp.Core

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Chirp.Repositories;
 
 namespace Chirp.Web.Pages;
 
--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Chirp.Database;
 using Chirp.Repositories;
 
 namespace Chirp.Web;
