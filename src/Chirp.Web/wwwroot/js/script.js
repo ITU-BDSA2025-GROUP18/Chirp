7d8f4c7846f16e1868c7429df0ba865c61495315
MillrDev <vmol@itu.dk>
2025-10-24 15:17:26 +0200

    Prev page event listener

--- src/Chirp.Web/wwwroot/js/script.js
+++ src/Chirp.Web/wwwroot/js/script.js
@@ -1,10 +1,18 @@
 ﻿const nextPageButton = document.querySelector(".next-page")
 const firstPageButton = document.querySelector(".first-page")
+const previousPageButton = document.querySelector(".previous-page")
 
 let currentPage = 1;
 
 nextPageButton.addEventListener("click", () => {
     currentPage++;
+    console.log(currentPage);
+})
+
+previousPageButton.addEventListener("click", () => {
+    if (currentPage > 1) {
+        currentPage--
+    }
     console.log(currentPage);
 })
 
