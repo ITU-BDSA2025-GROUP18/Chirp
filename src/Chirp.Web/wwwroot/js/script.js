886736a168ec049d5267f83b4e346955cdae7caf
MillrDev <vmol@itu.dk>
2025-10-24 15:11:58 +0200

    Added JS script,

---
+++ src/Chirp.Web/wwwroot/js/script.js
@@ -0,0 +1,13 @@
+﻿const nextPageButton = document.querySelector(".next-page")
+const firstPageButton = document.querySelector(".first-page")
+
+let currentPage = 1;
+
+nextPageButton.addEventListener("click", () => {
+    currentPage++;
+    console.log(currentPage);
+})
+
+firstPageButton.addEventListener("click", () => {
+    currentPage = 1;
+})
