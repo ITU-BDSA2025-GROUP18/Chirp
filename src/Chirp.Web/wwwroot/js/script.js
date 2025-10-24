ac1dc1560d9e934f693446bf1540c6034c6e0cb8
MillrDev <vmol@itu.dk>
2025-10-24 15:48:21 +0200

    Pagination works except for last page button

--- src/Chirp.Web/wwwroot/js/script.js
+++ src/Chirp.Web/wwwroot/js/script.js
@@ -1,21 +1,31 @@
 ﻿const nextPageButton = document.querySelector(".next-page")
 const firstPageButton = document.querySelector(".first-page")
 const previousPageButton = document.querySelector(".previous-page")
+const pageIndicator = document.querySelector(".page-indicator")
 
-let currentPage = 1;
+const searchParams = new URLSearchParams(window.location.search);
+let currentPage = searchParams.get('page');
+
+(currentPage == null) ? pageIndicator.innerHTML = "1" : pageIndicator.innerHTML = currentPage;
 
 nextPageButton.addEventListener("click", () => {
-    currentPage++;
-    console.log(currentPage);
+    if (searchParams.get('page') == null) {
+        currentPage += 2;
+    } else {
+        currentPage++;
+    }
+    document.location = `/?page=${currentPage}`
 })
 
 previousPageButton.addEventListener("click", () => {
     if (currentPage > 1) {
         currentPage--
+        document.location = `/?page=${currentPage}`
     }
-    console.log(currentPage);
 })
 
 firstPageButton.addEventListener("click", () => {
-    currentPage = 1;
+    if (currentPage > 1) {
+        document.location = "/?page=1"
+    }
 })
