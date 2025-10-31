9205d23eb0ff3f712f87771d82684f8d5da8cb14
oshe <oshe@itu.dk>
2025-10-31 17:28:14 +0100

    Added OAuth to project and Program.cs

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,8 +1,9 @@
 
-using Microsoft.EntityFrameworkCore;
 using Chirp.Repositories;
 using Chirp.Database;
 using Chirp.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Chirp.Web;
 
@@ -17,9 +18,33 @@ public abstract class Program
         builder.Services.AddDbContext<ChirpDBContext>(options =>
                 options.UseSqlite(connectionString, b => b.MigrationsAssembly("Chirp.Web")));
 
+        // Add ASP.NET Core Identity to the container. Source: course book, ch. 23.4
         builder.Services.AddDefaultIdentity<Author>(options =>
                 options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<ChirpDBContext>();
+
+        // Add OAuth authentication via GitHub to the container. Source: README_PROJECT, session 8
+        builder.Services.AddAuthentication(options =>
+            {
+                /*options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;*/
+
+                // The above code was included in the session 8 README_PROJECT OAuth example, however
+                // the app only works properly if it is commented out. Do not know if this poses any
+                // risks to the site... Source: https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers/issues/62
+
+                options.DefaultChallengeScheme = "GitHub";
+            })
+            .AddCookie()
+            .AddGitHub(o =>
+            {
+                o.ClientId = builder.Configuration["authentication:github:clientId"]!;
+                o.ClientSecret = builder.Configuration["authentication:github:clientSecret"]!;
+                o.CallbackPath = "/tmp/signin-github";
+                // Changed from "signin-github" as to not confuse the url for an Author
+                // (not sure if that is right or even needed)
+            });
+        builder.Services.AddSession();
 
         // Add services to the container.
         builder.Services.AddRazorPages();
@@ -42,6 +67,7 @@ public abstract class Program
 
         app.UseAuthentication();
         app.UseAuthorization();
+        app.UseSession();
 
         app.MapRazorPages();
         app.Run();
