b3589abe65ff19cf1b598e873371197debcb72b3
ITU-ahms <knahse4@knorddoc.dk>
2025-11-04 12:50:11 +0100

    tests: soft-check that page title mentions 'Chirp'

---
+++ tests/Chirp.Razor.Tests/TitleTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+public class TitleTests : IClassFixture<WebAppFactory>
+{
+    private readonly WebAppFactory _factory;
+    public TitleTests(WebAppFactory factory) => _factory = factory;
+
+    [Fact]
+    public async Task Root_Title_Contains_AppName()
+    {
+        var client = _factory.CreateClient();
+        var html = await client.GetStringAsync("/");
+        Assert.Contains("Chirp", html); // robust: ikke afhængig af præcis titeltekst
+    }
+}
