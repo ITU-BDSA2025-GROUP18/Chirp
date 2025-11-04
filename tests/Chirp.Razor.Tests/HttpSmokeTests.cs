a65f18d5d82355d443320af3ac4ad87e89f89a6b
ITU-ahms <knahse4@knorddoc.dk>
2025-11-04 12:50:10 +0100

    tests: add basic HTTP smoke test for '/' (expects 200 OK)

---
+++ tests/Chirp.Razor.Tests/HttpSmokeTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+public class HttpSmokeTests : IClassFixture<WebAppFactory>
+{
+    private readonly WebAppFactory _factory;
+    public HttpSmokeTests(WebAppFactory factory) => _factory = factory;
+
+    [Fact]
+    public async Task Root_Returns_OK()
+    {
+        var client = _factory.CreateClient();
+        var res = await client.GetAsync("/");
+        Assert.Equal(HttpStatusCode.OK, res.StatusCode);
+    }
+}
