5a31eae8248619c5c0a8dc0906d585d476c13075
MillrDev <millervinjr@proton.me>
2025-11-04 23:09:34 +0100

    Add getcheeps calls in public and usertimeline cshtml.cs

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -8,6 +8,7 @@ public class PublicModel : PageModel //All queries
 {
     private readonly ICheepRepository _repository;
     public List<CheepDTO> Cheeps { get; set; }
+    public int CheepsCount;
 
     public PublicModel(ICheepRepository repository)
     {
@@ -18,6 +19,7 @@ public class PublicModel : PageModel //All queries
     public async Task<ActionResult> OnGet([FromQuery] int page = 1)
     {
         Cheeps = await _repository.GetCheepsAsync(page);
+        CheepsCount = _repository.GetCheepsCountAsync().Result;
         return Page();
     }
 }
