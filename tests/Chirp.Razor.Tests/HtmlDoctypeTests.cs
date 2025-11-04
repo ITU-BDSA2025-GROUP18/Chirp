0dbb53eaf19d19cc03641af663d7b6f4e9c502e8
ITU-ahms <knahse4@knorddoc.dk>
2025-11-04 12:50:11 +0100

    tests: ensure '/' contains <!DOCTYPE html>

---
+++ tests/Chirp.Razor.Tests/HtmlDoctypeTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+public class HtmlDoctypeTests : IClassFixture<WebAppFactory>
+{
+    private readonly WebAppFactory _factory;
+    public HtmlDoctypeTests(WebAppFactory factory) => _factory = factory;
+
+    [Fact]
+    public async Task Root_Html_Starts_With_Doctype()
+    {
+        var client = _factory.CreateClient();
+        var html = await client.GetStringAsync("/");
+        Assert.Contains("<!DOCTYPE html", html);
+    }
+}
