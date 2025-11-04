ef730a83f16a4b702a74d53236262a006bd02e0a
ITU-ahms <knahse4@knorddoc.dk>
2025-11-04 12:50:11 +0100

    tests: assert HTML Content-Type for '/'

---
+++ tests/Chirp.Razor.Tests/ContentTypeTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+public class ContentTypeTests : IClassFixture<WebAppFactory>
+{
+    private readonly WebAppFactory _factory;
+    public ContentTypeTests(WebAppFactory factory) => _factory = factory;
+
+    [Fact]
+    public async Task Root_Has_Html_ContentType()
+    {
+        var client = _factory.CreateClient();
+        var res = await client.GetAsync("/");
+        res.EnsureSuccessStatusCode();
+        Assert.Contains("text/html", res.Content.Headers.ContentType?.MediaType);
+    }
+}
