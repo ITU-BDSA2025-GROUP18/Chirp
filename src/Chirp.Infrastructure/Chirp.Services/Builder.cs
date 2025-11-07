a7f2fc62d33e5c180b697141af57b019f5e3adf1
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-11-07 13:29:52 +0100

    Moved the program configuration to Services

---
+++ src/Chirp.Infrastructure/Chirp.Services/Builder.cs
@@ -0,0 +1,48 @@
+﻿using Chirp.Core;
+using Chirp.Database;
+using Chirp.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Web.CodeGeneration.Design;
+
+namespace Chirp.Services;
+
+public abstract class Builder
+{
+    public static WebApplicationBuilder Initialize(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Load database connection via configuration
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+        builder.Services.AddDbContext<ChirpDBContext>(options =>
+            options.UseSqlite(connectionString, b => b.MigrationsAssembly("Chirp.Web")));
+
+        // Add ASP.NET Core Identity to the container. Source: course book, ch. 23.4
+        builder.Services.AddDefaultIdentity<Author>(options =>
+                options.SignIn.RequireConfirmedAccount = true)
+            .AddSignInManager<EmailSignInManager>()
+            .AddEntityFrameworkStores<ChirpDBContext>();
+
+        // Add OAuth authentication via GitHub to the container. Source: README_PROJECT, session 8
+        builder.Configuration.AddUserSecrets<Program>();
+        builder.Services.AddAuthentication()
+            .AddCookie()
+            .AddGitHub(o =>
+            {
+                o.ClientId = builder.Configuration["authentication_github_clientId"]!;
+                o.ClientSecret = builder.Configuration["authentication_github_clientSecret"]!;
+                o.CallbackPath = "/signin-github";
+            });
+        builder.Services.AddSession();
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
+        return builder;
+    }
+}
--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -11,35 +11,10 @@ public abstract class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        // Configure the project with builder pattern
+        var builder = Builder.Initialize(args);
 
-        // Load database connection via configuration
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
-        builder.Services.AddDbContext<ChirpDBContext>(options =>
-            options.UseSqlite(connectionString, b => b.MigrationsAssembly("Chirp.Web")));
-
-        // Add ASP.NET Core Identity to the container. Source: course book, ch. 23.4
-        builder.Services.AddDefaultIdentity<Author>(options =>
-                options.SignIn.RequireConfirmedAccount = true)
-            .AddSignInManager<EmailSignInManager>()
-            .AddEntityFrameworkStores<ChirpDBContext>();
-
-        // Add OAuth authentication via GitHub to the container. Source: README_PROJECT, session 8
-        builder.Configuration.AddUserSecrets<Program>();
-        builder.Services.AddAuthentication()
-            .AddCookie()
-            .AddGitHub(o =>
-            {
-                o.ClientId = builder.Configuration["authentication_github_clientId"]!;
-                o.ClientSecret = builder.Configuration["authentication_github_clientSecret"]!;
-                o.CallbackPath = "/signin-github";
-            });
-        builder.Services.AddSession();
-
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-
+        // Build the application
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -49,7 +24,7 @@ public abstract class Program
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-
+        
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
