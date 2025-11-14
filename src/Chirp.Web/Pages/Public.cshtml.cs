0ae538f93da1a2d727ed59e7930d5d3f93614944
Casper <Casper.meier.jenkins@gmail.com>
2025-11-14 15:09:47 +0100

    added action to cshtml
    Co-Authored-By: MillrDev <92321245+MillrDev@users.noreply.github.com>

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -15,4 +15,24 @@ public class PublicModel(ICheepRepository repository, ChirpDBContext dbContext) 
         CheepsCount = Repository.GetCheepsCountAsync().Result;
         return Page();
     }
+
+    public async Task<IActionResult> OnPostFollowAsync()
+    {
+        Console.WriteLine("FOLLOWING HELGE");
+        var follower = await Repository.GetAuthorFromNameAsync(User.Identity!.Name!);
+        var followed = await Repository.GetAuthorFromNameAsync("Helge");
+        await Repository.FollowAsync(follower,followed);
+
+        return RedirectToPage();
+    }
+
+    public async Task<IActionResult> OnPostUnfollowAsync()
+    {
+        Console.WriteLine("UNFOLLOWING HELGE");
+        var follower = await Repository.GetAuthorFromNameAsync(User.Identity!.Name!);
+        var followed = await Repository.GetAuthorFromNameAsync("Helge");
+        await Repository.UnfollowAsync(follower,followed);
+
+        return RedirectToPage();
+    }
 }
