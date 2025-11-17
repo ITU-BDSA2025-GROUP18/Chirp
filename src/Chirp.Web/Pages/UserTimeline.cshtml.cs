0d9c6e6419e2501861b45386802e978f84730004
MillrDev <vmol@itu.dk>
2025-11-17 21:29:59 +0100

    Add following author's cheep to cheeps
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,17 +1,21 @@
-﻿using Chirp.Database;
+﻿using Chirp.Core;
+using Chirp.Database;
 using Microsoft.AspNetCore.Mvc;
 using Chirp.Repositories;
 using Chirp.Web.Pages.Shared;
 
 namespace Chirp.Web.Pages;
 
-public class UserTimelineModel(ICheepRepository repository, ChirpDBContext dbContext) : TimelineModel(repository, dbContext) //All queries
+public class UserTimelineModel(ICheepRepository repository, ChirpDBContext dbContext)
+    : TimelineModel(repository, dbContext) //All queries
 {
     public int AuthorCheepsCount;
 
     public async Task<ActionResult> OnGet(string author, [FromQuery] int page = 1)
     {
         Cheeps = await Repository.GetCheepsFromAuthorAsync(author, page);
+        //TODO add following uathor's cheeps appear in cheeps
+        foreach (var follower in Following) Cheeps.Union(await Repository.GetCheepsFromAuthorAsync(follower, page));
         AuthorCheepsCount = Repository.GetCheepsFromAuthorCountAsync(author).Result;
         return Page();
     }
