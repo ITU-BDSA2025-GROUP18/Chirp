b75950541de3014786e6a601f452d2cf6bef1721
MillrDev <millervinjr@proton.me>
2025-11-17 23:13:25 +0100

    ICHeepRepository cleanup + declaration

--- src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
@@ -7,8 +7,8 @@ public interface ICheepRepository
 {
     public Task<List<CheepDTO>> GetCheepsAsync(int page);
     public Task<int> GetCheepsCountAsync();
-    public Task<List<CheepDTO>> GetCheepsFromAuthorAsync(string authorName, int page);
-    public Task<int> GetCheepsFromAuthorCountAsync(string authorName);
+    public Task<int> GetCheepsFromAuthorsCountAsync(IEnumerable<string> authors);
+    public Task<List<CheepDTO>> GetCheepsFromAuthorsAsync(IEnumerable<string> authors, int page);
     public Task<Author?> GetAuthorFromNameAsync(string name);
     public Task<Author?> GetAuthorFromEmailAsync(string email);
     public Task<int> PostCheepAsync(Author author, int cheepId, string text);
