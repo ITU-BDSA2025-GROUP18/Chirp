da34731f7d1fbdb6ffe4cc7a409a879c0a4ac22a
MillrDev <millervinjr@proton.me>
2025-11-18 00:24:46 +0100

    Update ICheepRepository.cs
    Clean up file and changed declarations

--- src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/ICheepRepository.cs
@@ -15,4 +15,5 @@ public interface ICheepRepository
     public Task<int> FollowAsync(Author followingAuthor, Author followedAuthor);
     public Task<int> UnfollowAsync(Author followingAuthor, Author followedAuthor);
     public Task<HashSet<Followers>> AuthorFollowing(Author followingAuthor);
+    public Task<int> AuthorFollowersCount(Author author);
 }
