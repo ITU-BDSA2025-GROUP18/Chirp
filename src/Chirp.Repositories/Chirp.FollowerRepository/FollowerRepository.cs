9b11be3fba94e340b42fb1a7c95382e7d124b70b
oshe <oshe@itu.dk>
2025-11-28 18:25:30 +0100

    Create FollowerRepository

---
+++ src/Chirp.Repositories/Chirp.FollowerRepository/FollowerRepository.cs
@@ -0,0 +1,13 @@
+using Chirp.Database;
+
+namespace Chirp.Repositories.FollowerRepository;
+
+public class FollowerRepository(ChirpDBContext dbContext) : IFollowerRepository
+{
+    // ============== Get Endpoints ============== //
+
+
+    // ============== Post Endpoints ============== //
+
+
+}
