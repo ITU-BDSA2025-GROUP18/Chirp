f49f9e0d78ad5b33c8826a20b8c1bf088916d945
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-12-29 16:06:22 +0100

    Update Followers.cs

--- src/Chirp.Core/Followers.cs
+++ src/Chirp.Core/Followers.cs
@@ -1,13 +1,33 @@
-﻿
-namespace Chirp.Core;
+﻿namespace Chirp.Core;
 
 #nullable disable
 
+/// <summary>
+/// Represents a follower relationship between two authors in the Chirp system.
+/// </summary>
+/// <remarks>
+/// This class models the association where one author follows another.
+/// It includes both the IDs and usernames of the follower and the followed author.
+/// </remarks>
 public class Followers
 {
+    /// <summary>
+    /// Gets the unique identifier of the author who is following another author.
+    /// </summary>
     public string FollowingAuthorId { get; init; }
+
+    /// <summary>
+    /// Gets the username of the author who is following another author.
+    /// </summary>
     public string FollowingAuthorName { get; init; }
 
+    /// <summary>
+    /// Gets the unique identifier of the author being followed.
+    /// </summary>
     public string FollowedAuthorId { get; init; }
+
+    /// <summary>
+    /// Gets the username of the author being followed.
+    /// </summary>
     public string FollowedAuthorName { get; init; }
 }
