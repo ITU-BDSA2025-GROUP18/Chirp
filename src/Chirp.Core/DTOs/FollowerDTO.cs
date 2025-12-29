1a0a876193bdcd60bfec4df8b11c3e567453df03
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-12-29 16:02:42 +0100

    Update FollowerDTO.cs

--- src/Chirp.Core/DTOs/FollowerDTO.cs
+++ src/Chirp.Core/DTOs/FollowerDTO.cs
@@ -1,6 +1,19 @@
 namespace Chirp.Core.DTOs;
 
+/// <summary>
+/// Data Transfer Object (DTO) representing a follower relationship in the Chirp system.
+/// </summary>
+/// <remarks>
+/// This DTO is used to transfer information about a user who follows another user,
+/// without exposing internal domain or database models.
+/// </remarks>
 public class FollowerDTO
 {
+    /// <summary>
+    /// Gets or sets the username of the follower.
+    /// </summary>
+    /// <remarks>
+    /// This value identifies the user who is following another author within the Chirp platform.
+    /// </remarks>
     public required string FollowerName;
 }
