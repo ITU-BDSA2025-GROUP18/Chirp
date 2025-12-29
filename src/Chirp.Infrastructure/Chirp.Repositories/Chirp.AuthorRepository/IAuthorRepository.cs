901f5942514c3946429728bd22f1614116996ab2
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-12-29 16:08:59 +0100

    Update IAuthorRepository.cs

--- src/Chirp.Infrastructure/Chirp.Repositories/Chirp.AuthorRepository/IAuthorRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/Chirp.AuthorRepository/IAuthorRepository.cs
@@ -3,9 +3,41 @@ using Chirp.Core.DTOs;
 
 namespace Chirp.Repositories;
 
+/// <summary>
+/// Defines the contract for author-related data access operations.
+/// </summary>
+/// <remarks>
+/// This interface provides methods to retrieve author information
+/// from the database, either as domain models or as Data Transfer Objects (DTOs).
+/// Implementations of this interface handle data retrieval logic for authors.
+/// </remarks>
 public interface IAuthorRepository
 {
+    /// <summary>
+    /// Retrieves the personal data of an author by username.
+    /// </summary>
+    /// <param name="authorName">The username of the author.</param>
+    /// <returns>
+    /// A <see cref="AuthorDTO"/> containing the author's personal data,
+    /// or <c>null</c> if the author is not found.
+    /// </returns>
     public Task<AuthorDTO?> GetPersonalDataAsync(string authorName);
+
+    /// <summary>
+    /// Retrieves an <see cref="Author"/> entity by username.
+    /// </summary>
+    /// <param name="name">The username of the author.</param>
+    /// <returns>
+    /// The <see cref="Author"/> entity if found; otherwise, <c>null</c>.
+    /// </returns>
     public Task<Author?> GetAuthorFromNameAsync(string name);
+
+    /// <summary>
+    /// Retrieves an <see cref="Author"/> entity by email address.
+    /// </summary>
+    /// <param name="email">The email address of the author.</param>
+    /// <returns>
+    /// The <see cref="Author"/> entity if found; otherwise, <c>null</c>.
+    /// </returns>
     public Task<Author?> GetAuthorFromEmailAsync(string email);
 }
